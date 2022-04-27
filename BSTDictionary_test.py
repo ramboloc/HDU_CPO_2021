1ca3b0706e0fb23f2dc21d826f0cca3929f89e3d
Bin <1255985865@qq.com>
2022-04-27 21:32:39 +0800

    fix the model naming and coverage setting

--- BSTDictionary_test.py
+++ BSTDictionary_test.py
@@ -3,55 +3,10 @@ from hypothesis import given
 import hypothesis.strategies as st
 import BSTDictionary as td
 from BSTDictionary import BSTDictionary
+import external_library as el
 
 
-def f(x):
-    """"this function transform the type of x to str """
-    return str(x)
-
-
-def add_value(obj, value):
-    """this function add all value if type of value is int """
-    if type(value) is int:
-        return obj + value
-    else:
-        return obj
-
-
-def list_to_kv_list(list1):
-    """
-    Convert a list containing two tuples into kV we require_ list
-    kv_ List shall have the following characteristics:
-    1. All elements are binary.
-    2. All element key values are not duplicate.
-    :param list1:
-    :return:
-    """
-    list_key = []
-    list_value = []
-    new_list = []
-    for i in list1:
-        if list_key.__contains__(i[0]) is not True:
-            list_key.append(i[0])
-            list_value.append(i[1])
-        else:
-            list_value[list_key.index(i[0])] = i[1]
-    index = 0
-    for key in list_key:
-        new_list.append((key, list_value[index]))
-        index = index + 1
-    new_list.sort()
-    return new_list
-
-
-def list_merge(lis1, lis2):
-    """merge tow list to one according to list length"""
-    for i in lis2:
-        lis1.append(i)
-    return lis1
-
-
-class TestMutableDictionary(unittest.TestCase):
+class TestBSTDictionary(unittest.TestCase):
 
     def test_set(self):
         dictionary_test = BSTDictionary()
@@ -120,7 +75,7 @@ class TestMutableDictionary(unittest.TestCase):
         dictionary_test = BSTDictionary()
         dictionary_test.put(1, 11)
         dictionary_test.put(2, 22)
-        dictionary_test.map(f)
+        dictionary_test.map(el.f)
         self.assertEqual(dictionary_test.to_list(), [(1, "11"), (2, "22")])
 
     def test_reduce(self):
@@ -128,12 +83,12 @@ class TestMutableDictionary(unittest.TestCase):
         dictionary_test.put(1, 11)
         dictionary_test.put(2, "22")
         dictionary_test.put(3, 33)
-        result = dictionary_test.reduce(add_value, initial_state=0)
+        result = dictionary_test.reduce(el.add_value, initial_state=0)
         self.assertEqual(result, 44)
 
     @given(st.lists(st.tuples(st.integers(), st.integers())))
     def test_empty(self, list1):
-        new_li = list_to_kv_list(list1)
+        new_li = el.list_to_kv_list(list1)
         dictionary_test = BSTDictionary()
         dictionary_test.from_list(list1)
         self.assertEqual(dictionary_test.to_list(), new_li)
@@ -146,10 +101,10 @@ class TestMutableDictionary(unittest.TestCase):
         dictionary_test = BSTDictionary()
         dictionary_test2 = BSTDictionary()
         if len(list1) > len(list2):
-            new_list = list_merge(list1, list2)
+            new_list = el.list_merge(list1, list2)
         else:
-            new_list = list_merge(list2, list1)
-        new_list = list_to_kv_list(new_list)
+            new_list = el.list_merge(list2, list1)
+        new_list = el.list_to_kv_list(new_list)
         dictionary_test.from_list(list1)
         dictionary_test2.from_list(list2)
         dictionary_test.concat(dictionary_test2)
---
+++ external_library.py
@@ -0,0 +1,44 @@
+def f(x):
+    """"this function transform the type of x to str """
+    return str(x)
+
+
+def add_value(obj, value):
+    """this function add all value if type of value is int """
+    if type(value) is int:
+        return obj + value
+    else:
+        return obj
+
+
+def list_to_kv_list(list1):
+    """
+    Convert a list containing two tuples into kV we require_ list
+    kv_ List shall have the following characteristics:
+    1. All elements are binary.
+    2. All element key values are not duplicate.
+    :param list1:
+    :return:
+    """
+    list_key = []
+    list_value = []
+    new_list = []
+    for i in list1:
+        if list_key.__contains__(i[0]) is not True:
+            list_key.append(i[0])
+            list_value.append(i[1])
+        else:
+            list_value[list_key.index(i[0])] = i[1]
+    index = 0
+    for key in list_key:
+        new_list.append((key, list_value[index]))
+        index = index + 1
+    new_list.sort()
+    return new_list
+
+
+def list_merge(lis1, lis2):
+    """merge tow list to one according to list length"""
+    for i in lis2:
+        lis1.append(i)
+    return lis1
